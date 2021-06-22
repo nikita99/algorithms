0fb00dc274fbdee8faf5469b343ad2274ad3e2ea
nikita99 <nikitadhole99@gmail.com>
2021-06-22 15:24:00 +0530

    Create solution.java

---
+++ arrays/Buy and sell stocks/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        if(prices.length == 1){
+            return maxProfit;
+        }
+        int i = 0, buy = prices[0];
+        while(i<prices.length - 1){
+            if(prices[i+1]<=prices[i]){
+                while(i<prices.length-1 && prices[i+1]<=prices[i])
+                    i++;
+            }
+            buy = prices[i];
+            if(i<prices.length-1 && prices[i+1]>prices[i]){
+                while(i<prices.length-1 && prices[i+1]>prices[i])
+                    i++;
+                maxProfit += prices[i] - buy;
+            }
+            i++;
+        }
+        return maxProfit;
+    }
+}
